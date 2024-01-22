1ed930dfa1327faa7f38e69b64906158696c69eb
gromyko <vl-mailbox@mail.ru>
2024-01-22 10:17:41 +0300

    Added customer table.

--- app.constants.ts
+++ app.constants.ts
@@ -2,6 +2,6 @@
 export const APP_WRITE_ID = 'crm-system-nuxt-project-vlad'
 export const DB_ID = 'crm-base'
 export const COLLECTION_DEALS = 'deals'
-export const COLLECTION_CUSTOMER = 'customers'
+export const COLLECTION_CUSTOMERS = 'customers'
 export const COLLECTION_COMMENTS = 'comments'
 export const STORAGE_ID = 'storage'
